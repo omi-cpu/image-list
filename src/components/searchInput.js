53b227804c6cd427beb4dce7d14a79693c884ace
omi-cpu <obriggs53@gmail.com>
2021-12-01 19:03:50 +0100

    manipulating 'this' keyword

--- src/components/searchInput.js
+++ src/components/searchInput.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 class SearchInput extends React.Component{
 
+    constructor(props){
+        super(props)
+        this.state={entry: ''}
+
+
+        //this.onFormSubmit = this.onFormSubmit.bind(this)
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault(); //this prevent the page from reloading after submitting the form
+        console.log(this.state.entry);
+    }
+
     render(){
         return(
             <div className="ui segment">
-                <form action="" className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="ui field">
                         <div className="ui massive icon input">
-                        <input type="text" placeholder="search" onChange={(event) => console.log(event.target.value)}/>
+                        <input type="text" placeholder="search" onChange={(event) => this.setState({entry:event.target.value})} value={this.state.entry}/>
                         <i className="search icon"></i>
                         </div>
                     </div>
