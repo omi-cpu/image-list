d43be482a5ce9f5ed18c45c85913d4e30351a12e
omi-cpu <obriggs53@gmail.com>
2021-12-02 22:16:31 +0100

    async await function

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,14 @@
 import React from "react";
+import axios from "axios";
 import SearchInput from './searchInput';
 
 
 class App extends React.Component{
 
-    constructor(props){
-        super(props)
-
-        this.onSearchSubmit =this.onSearchSubmit.bind(this)
+    async onSearchSubmit(entry){
+        const response = await axios.get(`https://pixabay.com/api/?key=24632924-feb67c34b16f046333ef4e22d&q=${entry}&image_type=photo&pretty=true`)
+        console.log(response)
     }
-
-    onSearchSubmit(entry){
-        console.log(entry)
-         }
 
     render(){
         return(
